aa3261c317f3e075f195b5e6191b0e32fb80ce99
h-corbin <macbookpro@Guys-MacBook-Pro.local>
2022-07-19 18:30:37 -0400

    started Angular app

--- inventory-management/src/main/java/com/skillstorm/servlets/WarehouseServlet.java
+++ inventory-management/src/main/java/com/skillstorm/servlets/WarehouseServlet.java
@@ -1,6 +1,8 @@
 package com.skillstorm.servlets;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,21 +10,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skillstorm.daos.MySQLWarehouseDAOImpl;
+import com.skillstorm.daos.WarehouseDAO;
+import com.skillstorm.models.Warehouse;
+
 @WebServlet(urlPatterns = "/warehouse")
 public class WarehouseServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -1255978588645665829L;
+	
+	WarehouseDAO dao = new MySQLWarehouseDAOImpl();
+	ObjectMapper mapper = new ObjectMapper();
+	
+	
 
 	@Override
+	// return all 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("Hello");
-		super.doGet(req, resp);
+		List<Warehouse> warehouseList = dao.findall()
+		resp.setContentType("application/json");
+		resp.getWriter().print(mapper.writeValueAsString(warehouseList));
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		
+		InputStream reqBody = req.getInputStream(); // get JSON request body
+		
+		Warehouse warehouse = mapper.readValue(reqBody, Warehouse.class); // translate to Warehouse object
+		System.out.println(warehouse);
+		
+		resp.setContentType("application/json");
+		resp.getWriter().print(mapper.writeValueAsString(warehouse));
 	}
 	
 	@Override
